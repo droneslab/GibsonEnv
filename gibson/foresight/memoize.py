620ad705a6059376c60d3c851daafca282c01da2
Charuvahan Adhivarahan <charuvah@buffalo.edu>
2019-11-11 11:41:26 -0500

    ...

--- gibson/foresight/memoize.py
+++ gibson/foresight/memoize.py
@@ -1,11 +1,73 @@
 import os
 from functools import wraps
+
+import numpy as np
+
 import pickle as pkl
+
+
+class Memoize(object):
+    """Memoizer for RL training"""
+    def __init__(self, path):
+        self.ssd = SSDStore(path)
+        self.ram = RAMStore()
+        self.ram_max_count = 1000
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped(*args):
+            result = None
+            key = self.generate_key(args[1])
+            result = self.get(key)
+            if result is None:
+                result = func(*args)
+                self.set(key, result)
+            return result
+
+        return wrapped
+
+    def generate_key(self, pose):
+        position = pose[0] * 100
+        orientation = np.array(pose[1], dtype=np.float64) * 100
+        return '{:.0f}_{:.0f}_{:.0f}_{:.0f}_{:.0f}_{:.0f}_{:.0f}'.format( position[0]
+                                                                        , position[1]
+                                                                        , position[2]
+                                                                        , orientation[0]
+                                                                        , orientation[1]
+                                                                        , orientation[2]
+                                                                        , orientation[3])
+
+    def get(self, key):
+        """Retrieve the value in cache against key
+
+        :key: key used for storage and retrieval
+        :returns: content stored against the key
+        """
+        if self.ram.exists(key):
+            return self.ram.get(key)
+        elif self.ssd.exists(key):
+            return self.ssd.get(key)
+        return None
+
+    def set(self, key, value):
+        """Store the value in cache against key
+
+        :key: key used for storage and retrieval
+        :value: content to be stored
+        """
+        if self.ram.count() < self.ram_max_count:
+            self.ram.set(key, value)
+        else:
+            self.ssd.set(key, value)
+
 
 class Store(object):
     """Abstract storage class"""
     def __init__(self):
         super(Store, self).__init__()
+
+    def exists(self, key):
+        pass
 
     def get(self, key):
         pass
@@ -24,30 +86,6 @@ class Store(object):
 
     def __exit__(self):
         self.close()
-
-
-class GPUStore(Store):
-    """GPU storage class"""
-    def __init__(self):
-        super(GPUStore, self).__init__()
-
-    def get(self, key):
-        pass
-
-    def set(self, key, value):
-        pass
-
-
-class RAMStore(Store):
-    """RAM storage class"""
-    def __init__(self):
-        super(RAMStore, self).__init__()
-
-    def get(self, key):
-        pass
-
-    def set(self, key, value):
-        pass
 
 
 class SSDStore(Store):
@@ -70,50 +108,31 @@ class SSDStore(Store):
             pkl.dump(value, fh)
 
 
-class Memoize(object):
-    """Memoizer for RL training"""
-    def __init__(self, path):
-        self.ssd = SSDStore(path)
+class RAMStore(Store):
+    """RAM storage class"""
+    def __init__(self):
+        super(RAMStore, self).__init__()
+        self.storage = dict()
 
-    def __call__(self, func):
-        @wraps(func)
-        def wrapped(*args):
-            result = None
-            key = self.generate_key(args[1])
-            if self.exists(key):
-                result = self.get(key)
-            else:
-                result = func(*args)
-                self.set(key, result)
-            return result
-
-        return wrapped
-
-    def generate_key(self, pose):
-        position = pose[0] * 100
-        return '{:.0f}_{:.0f}_{:.0f}'.format(position[0], position[1], position[2])
-
-    def exists(self, key):
-        """Check if key exists in cache
-
-        :key: key used for storage and retrieval
-        :returns: True if key exists, False otherwise
-        """
-        return self.ssd.exists(key)
-
+    def count(self):
+        return len(self.storage.keys())
 
     def get(self, key):
-        """Retrieve the value in cache against key
-
-        :key: key used for storage and retrieval
-        :returns: content stored against the key
-        """
-        return self.ssd.get(key)
+        return self.storage[key]
 
     def set(self, key, value):
-        """Store the value in cache against key
+        self.storage[key] = value
 
-        :key: key used for storage and retrieval
-        :value: content to be stored
-        """
-        self.ssd.set(key, value)
+
+class GPUStore(Store):
+    """GPU storage class"""
+    def __init__(self):
+        super(GPUStore, self).__init__()
+
+    def get(self, key):
+        pass
+
+    def set(self, key, value):
+        pass
+
+
